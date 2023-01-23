97a65d6fbf41aab3e59ddea931297e8ca1918600
DanaAloqily <danaaloqily@hotmail.com>
2023-01-23 16:45:19 +0300

    product-list & product-item

--- src/app/components/product-item/product-item.component.ts
+++ src/app/components/product-item/product-item.component.ts
@@ -1,10 +1,40 @@
-import { Component } from '@angular/core';
-
+import { Component, OnInit, Input, Output, EventEmitter } from "@angular/core";
+import { Product } from "src/app/models/Product";
 @Component({
-  selector: 'app-product-item',
-  templateUrl: './product-item.component.html',
-  styleUrls: ['./product-item.component.css']
+  selector: "app-product-item",
+  templateUrl: "./product-item.component.html",
+  styleUrls: ["./product-item.component.css"],
 })
-export class ProductItemComponent {
+export class ProductItemComponent implements OnInit {
+  @Input() product: Product;
+  amount = 0;
+  options = [
+    { value: 1 },
+    { value: 2 },
+    { value: 3 },
+    { value: 4 },
+    { value: 5 },
+    { value: 6 },
+    { value: 7 },
+    { value: 8 },
+    { value: 9 },
+    { value: 10 },
+  ];
 
+  constructor() {
+    this.product = {
+      id: -1,
+      name: "",
+      price: 0,
+      url: "https://images.unsplash.com/photo-1553062407-98eeb64c6a62?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=800&q=80",
+      description: "",
+    };
+  }
+
+  ngOnInit(): void {}
+  onAmountSelect(value: number): void {
+    this.amount = value;
+  }
+
+  addToCart(): void {}
 }
--- src/app/components/product-list/product-list.component.ts
+++ src/app/components/product-list/product-list.component.ts
@@ -1,10 +1,21 @@
-import { Component } from '@angular/core';
-
+import { Component, OnInit } from "@angular/core";
+import { Product } from "src/app/models/Product";
+import { ProductService } from "src/app/services/product.service";
 @Component({
-  selector: 'app-product-list',
-  templateUrl: './product-list.component.html',
-  styleUrls: ['./product-list.component.css']
+  selector: "app-product-list",
+  templateUrl: "./product-list.component.html",
+  styleUrls: ["./product-list.component.css"],
 })
-export class ProductListComponent {
+export class ProductListComponent implements OnInit {
+  products: Product[] = [];
+  constructor(private productService: ProductService) {}
 
+  ngOnInit(): void {
+    this.productService.getProduct().subscribe((res) => {
+      for (let i = 0; i < res.length; i++) {
+        const product = res[i];
+      }
+      this.products = res;
+    });
+  }
 }
